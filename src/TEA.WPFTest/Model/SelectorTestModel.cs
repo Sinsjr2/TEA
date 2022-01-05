a4f003177ae70d7f36ba9774e0465d83b15b4445
Sinsjr2 <hks15sirubia@gmail.com>
2022-01-05 23:06:52 +0900

    fix ComboBox does not apply selected index when items same size. and add test.
    ex: current [0, 1, 2] render values [3, 4, 5]

--- src/TEA.WPFTest/Model/SelectorTestModel.cs
+++ src/TEA.WPFTest/Model/SelectorTestModel.cs
@@ -10,6 +10,7 @@ namespace TEA.WPFTest.Model {
     public record SelectorTestModel : IUpdate<SelectorTestModel, ISelectorTestMessage> {
 
         int ListBoxPattern { get; init; } = 0;
+        int Pattern { get; init; } = 0;
 
         public ComboBoxModel<ListItem> SelectionAndItems => ListBoxPattern switch {
             0 => new(ListBoxPattern, new ListItem[] { new("this is 0", 0) }),
@@ -17,7 +18,11 @@ namespace TEA.WPFTest.Model {
             2 => new(ListBoxPattern, new ListItem[] { new("this is 2", 2), new("x2_2", 2), new("x3", 3) }),
             3 => new(ListBoxPattern, new ListItem[] { new("sample", 100), new("sample", 100), new("sample", 100), new("sample", 100), new("sample", 100) }),
             4 => new(ListBoxPattern, new ListItem[] { new("sample", 100), new("sample", 100), new("sample", 100), new("sample", 100), new("sample", 100), new("sample", 100) }),
-            5 => new(2             , new ListItem[] { new("other", 10), new("x2_3", 2), new("x3_2", 3), new("x4", 4) })
+            5 => new(2             , new ListItem[] { new("other", 10), new("x2_3", 2), new("x3_2", 3), new("x4", 4) }),
+            6 => new(6             ,
+                     Pattern == 0
+                     ? new ListItem[] { new("0", 0), new("1", 1), new("2", 2), new("3", 3), new("4", 4), new("5", 5), new("6", 6), }
+                     : new ListItem[] { new("A", 0xA), new("B", 0xB), new("C", 0xC), new("D", 0xD), new("E", 0xE), new("F", 0xF), new("G", 0xA1), }),
         };
 
         public ComboBoxModel<string> ForComboBox {
@@ -38,6 +43,7 @@ namespace TEA.WPFTest.Model {
                     _ => this with { ListBoxPattern = 0 }
                 },
                 OnClickChangeTo3Button msg => this with { ListBoxPattern = 3 },
+                    TogglePattern => this with { ListBoxPattern = 6, Pattern = (Pattern + 1) % 2 },
                 _ => this
             };
         }
